08a7afa5ea4707c55ef5ccf6cbf62b7eb0b84020
pintiliea <pintiliea81@gmail.com>
2017-07-11 22:54:52 +0200

    ro symbols file requires another strategy

--- installer.py
+++ installer.py
@@ -4,7 +4,7 @@ import logging.config
 from shutil import copy2
 
 logging.config.fileConfig('logging.conf')
-logger = logging.getLogger('popak-installer')
+logger = logging.getLogger('installer')
 
 # constants ------------------------------------------------------
 
@@ -20,7 +20,7 @@ RULES_EVDEV_LST = DIR_RULES + 'evdev.lst'
 SYMBOLS_RO = DIR_SYMBOLS + 'ro'
 EXT_BACKUP = '.backup'
 
-EVDEV_XML_FRAGMENT = '''        <variant>
+XML_FRAGMENT = '''        <variant>
           <configItem>
             <name>popak_standard</name>
             <description>Popak (Standard)</description>
@@ -34,6 +34,81 @@ EVDEV_XML_FRAGMENT = '''        <variant>
         </variant>
 '''
 
+LST_FRAGMENT = '''  popak_standard  ro: Popak Standard
+  popak_cedilla   ro: Popak Cedilla
+'''
+
+SYMBOLS_RO_FRAGMENT = '''
+partial default alphanumeric_keys
+xkb_symbols "popak_standard" {
+// Popak layout, developed by Nicolae Popa (nicolae.m.popa@gmail.com)
+// http://www.invatasingur.ro/popak
+// Standard variation (with correct diacritics)
+
+    include "us"
+
+    name[Group1]="Romania";
+
+    key <AE01> { [ 1,                 exclam,  section			  ] };
+    key <AE02> { [ 2,                     at,  copyright		  ] };
+    key <AE03> { [ 3,             numbersign,  guillemotleft 	  	  ] };
+    key <AE04> { [ 4,                 dollar,  guillemotright		  ] };
+    key <AE05> { [ 5,                percent,  degree			  ] };
+    key <AE11> { [ endash,	     	   question			  ] };
+    key <AD01> { [ b,                      B,  plusminus		  ] };
+    key <AD02> { [ p,                      P,  underscore 	          ] };
+    key <AD03> { [ c,                      C,  braceleft  	          ] };
+    key <AD04> { [ m,                      M,  braceright		  ] };
+    key <AD05> { [ v,                      V,  bar			  ] };
+    key <AD06> { [ acircumflex,            Acircumflex			  ] };
+    key <AD07> { [ icircumflex,            Icircumflex			  ] };
+    key <AD08> { [ abreve,                 Abreve			  ] };
+    key <AD09> { [ d,                      D	 			  ] };
+    key <AD10> { [ f,                      F	 			  ] };
+    key <AD11> { [ w,                      W	 			  ] };
+    key <AD12> { [ k,                      K	 			  ] };
+    key <AC01> { [ l,                      L,  doublelowquotemark         ] };
+    key <AC02> { [ t,                      T,  rightdoublequotemark	  ] };
+    key <AC03> { [ r,                      R,  less 			  ] };
+    key <AC04> { [ n,                      N,  greater 			  ] };
+    key <AC05> { [ s,                      S,  EuroSign 		  ] };
+    key <AC06> { [ o,                      O	 			  ] };
+    key <AC07> { [ a,                      A	 			  ] };
+    key <AC08> { [ e,                      E	 			  ] };
+    key <AC09> { [ i,                      I	 			  ] };
+    key <AC10> { [ u,                      U	 			  ] };
+    key <AC11> { [ z,                      Z	 			  ] };
+    key <LSGT> { [ q,            	   Q                              ] };
+    key <BKSL> { [ q,            	   Q                              ] };
+    key <AB01> { [ y,                      Y,  apostrophe 	       	  ] };
+    key <AB02> { [ 0x1000219,      0x1000218,  quotedbl 	       	  ] };
+    key <AB03> { [ 0x100021b,      0x100021a,  bracketleft 	       	  ] };
+    key <AB04> { [ h,                      H,  bracketright 	       	  ] };
+    key <AB05> { [ x,                      X,  backslash 	       	  ] };
+    key <AB06> { [ j,                      J		 	       	  ] };
+    key <AB07> { [ g,                      G		 	       	  ] };
+    key <AB08> { [ comma, 	   semicolon		          	  ] };
+    key <AB09> { [ period,             colon 	      		    	  ] };
+    key <AB10> { [ minus,              slash 	      		    	  ] };
+
+    include "level3(ralt_switch)"
+};
+
+partial alphanumeric_keys
+xkb_symbols "popak_cedilla" {
+    // Variant of the Popak layout with cedillas.
+    // Implements S and T with cedilllas as in ISO-8859-2.
+    // Included for compatibility reasons.
+
+    include "ro(popak_standard)"
+
+    name[Group1]="Romania";
+
+    key <AB02> { [ scedilla,      Scedilla,  quotedbl 	       	  	  ] };
+    key <AB03> { [ tcedilla,      Tcedilla,  bracketleft 	       	  ] };
+};
+
+'''
 
 # methods --------------------------------------------------------
 
@@ -44,7 +119,28 @@ def backup(file):
     copy2(file, file_backup)
 
 
+def match_replace(file_path, signal_str, append_to_str, replace_str):
+    match = False
+    with fileinput.FileInput(file_path, inplace=True) as f:
+        for line in f:
+            if signal_str in line:
+                match = True
+            if match:
+                if append_to_str in line:
+                    line = line.replace(line, line + replace_str)
+                    # logger.debug(line)
+                    print(line, end='')
+                    match = False
+                else:
+                    print(line, end='')
+            else:
+                print(line, end='')
+
+
 # installation logic ---------------------------------------------
+
+# TODO: detect already installed popak and stop
+# TODO: backup incremental if .backup already exists -> .backup2
 
 # backup(RULES_BASE_XML)
 # backup(RULES_BASE_LST)
@@ -55,23 +151,11 @@ def backup(file):
 # backup(SYMBOLS_RO)
 
 
-evdev_xml_match = False
-evdev_lst_match = False
+# match_replace(RULES_EVDEV_XML, 'Romanian (WinKeys)', '</variant>', XML_FRAGMENT)
+# match_replace(RULES_BASE_XML, 'Romanian (WinKeys)', '</variant>', XML_FRAGMENT)
 
-base_xml_match = False
-base_lst_match = False
+# match_replace(RULES_EVDEV_LST, 'Romanian (WinKeys)', 'Romanian (WinKeys)', LST_FRAGMENT)
+# match_replace(RULES_BASE_LST, 'Romanian (WinKeys)', 'Romanian (WinKeys)', LST_FRAGMENT)
 
-with fileinput.FileInput(RULES_EVDEV_XML, inplace=True) as f:
-    for line in f:
-        if 'Romanian (WinKeys)' in line:
-            evdev_xml_match = True
-        if evdev_xml_match:
-            if '</variant>' in line:
-                line = line.replace(line, line + EVDEV_XML_FRAGMENT)
-                logger.debug(line)
-                print(line, end='')
-                evdev_xml_match = False
-            else:
-                print(line, end='')
-        else:
-            print(line, end='')
+# TODO: use regex here?
+match_replace(SYMBOLS_RO, 'xkb_symbols "winkeys"', 'partial', SYMBOLS_RO_FRAGMENT)
